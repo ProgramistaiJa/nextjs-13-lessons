1b1e3cab78c0c77651e058b6e07fc3d44dbe1d66
ProgramistaiJa <programista@i-ja.pl>
2023-07-21 05:54:35 +0200

    lesson 2

---
+++ app/about/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+    title: 'About',
+    description: 'About Next.js',
+  }
+
+export default function AboutLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+  return (
+    <>
+        <nav>About NavBar</nav>
+        <main className="flex min-h-screen flex-col items-center justify-start p-24">
+            {children}
+        </main>
+    </>
+  )
+}
